4f322bd31a57e33ce36fdc320b9b5c0c69a640c7
Olga <notist@mail.ru>
2016-12-21 11:42:52 +0300

    started second part

--- js/main.js
+++ js/main.js
@@ -32,7 +32,6 @@ var controller = new ScrollMagic.Controller({
     }
 })
 
- 
 
 	function pathPrepare ($el) {
 		var lineLength = $el[0].getTotalLength();
@@ -40,14 +39,17 @@ var controller = new ScrollMagic.Controller({
 		$el.css("stroke-dashoffset", lineLength);
 	}
 
-	//var $word = $("path#word");
-	//var $dot = $("path#dot");
-	var $line1 = $("polyline#line1");
-	var $test = $("path#test");
+	
+	var $line1 = $("path#line1");
+	var $second_line1 = $("path#second_line1");
+	var $second_line2 = $("path#second_line2");
+	var $second_line3 = $("path#second_line3");
+
 	// prepare SVG
-//	pathPrepare($word);
-//	pathPrepare($dot);
-	pathPrepare($test);
+	pathPrepare($line1);
+	pathPrepare($second_line1);
+	pathPrepare($second_line2);
+	pathPrepare($second_line3);
 
  new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 400})
 						.setPin("#one", {pushFollowers:true})		
@@ -63,8 +65,7 @@ var controller = new ScrollMagic.Controller({
 	// build tween
 	var tween = new TimelineMax()
 		.add(TweenMax.to($line1, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
-		//.add(TweenMax.to($dot, 0.1, {strokeDashoffset: 0, ease:Linear.easeNone}))  // draw dot for 0.1
-		//.add(TweenMax.to("path", 1, {stroke: "#33629c", ease:Linear.easeNone}), 0);			// change color during the whole thing
+		
 
 	// build scene
 	new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 400, tweenChanges: true})
@@ -81,7 +82,7 @@ var controller = new ScrollMagic.Controller({
 
 /********************************************************/
 
- new ScrollMagic.Scene({triggerElement: "#trigger2", duration:700})
+ new ScrollMagic.Scene({triggerElement: "#trigger2", duration:800})
 						.setPin("#two", {pushFollowers:true})		
 						.addIndicators({
 						    colorStart: "rgba(255,255,255,0.5)",
@@ -94,11 +95,13 @@ var controller = new ScrollMagic.Controller({
 
 	// build tween
 	var tween = new TimelineMax()
-		.add(TweenMax.to($test, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
-		.add(TweenMax.to("path", 1, {stroke: "#33629c", ease:Linear.easeNone}), 0);			// change color during the whole thing
+		.add(TweenMax.to($second_line1, 0.5, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		.add(TweenMax.to($second_line2, 0.6, {strokeDashoffset: 0, ease:Linear.easeNone}))  
+		.add(TweenMax.to($second_line3, 0.7, {strokeDashoffset: 0, ease:Linear.easeNone}))  
+		//.add(TweenMax.to("path", 1, {stroke: "#33629c", ease:Linear.easeNone}), 0);	// change color during the whole thing
 
 	// build scene
-	new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 300, tweenChanges: true})
+	new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 400, tweenChanges: true})
 					.setTween(tween)		
 					.addIndicators({
 					    colorStart: "rgba(255,255,255,0.5)",
